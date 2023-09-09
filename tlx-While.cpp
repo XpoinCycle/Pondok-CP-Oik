5c85533a4d073428c9cae10f232055c3184379ea
XpoinCycle <144209929+XpoinCycle@users.noreply.github.com>
2023-09-09 22:01:27 +0700

    Create tlx-While.cpp

---
+++ tlx-While.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string Input, InputList[101];
+    int m,n = 0;
+    
+    while (cin >> InputList[n]){
+        n++; 
+    }
+    while (m < n){
+        cout << InputList[m] << endl;;
+        m++;
+    }
+    
+
+}
